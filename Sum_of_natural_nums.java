a882309c4fe10b34942a1414e4dbe8636db73333
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-21 10:48:29 -0700

    Create Sum_of_natural_nums.java

---
+++ Sum_of_natural_nums.java
@@ -0,0 +1,18 @@
+//Sum of natural nums
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num = 10 , sum = 0;
+		for(int i = 1; i <= num; ++i) {
+			sum += i;
+		}
+		System.out.println("Sum is: "+sum);
+
+	}
+
+}
+
+/* Output :
+Sum is: 55
+*/
