fe003b9caa4c765d4e50ec2f846e16fe708908ec
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-20 20:18:54 -0700

    Create add_two_integers.java

---
+++ add_two_integers.java
@@ -0,0 +1,18 @@
+//Sum of two numbers
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int v1 = 10;
+		int v2 = 80;
+		int sum = v1 + v2;
+		System.out.println("Addition of two numbers is: "+sum);
+		
+	}
+
+}
+
+/* Output :
+Addition of two numbers is: 90
+/*
