db12bc650d790491432fd8e335d52b39aefa4834
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-20 20:14:45 -0700

    print_integer

---
+++ print_integer.java
@@ -0,0 +1,22 @@
+//Java Program to Print an Integer (Entered by the User)
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter val: ");
+		int val = sc.nextInt();
+		System.out.println(val);
+		
+	}
+
+}
+
+/* OUTPUT: 
+
+Enter val: 
+23
+23
+/*
