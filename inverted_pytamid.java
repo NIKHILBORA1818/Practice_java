53da48f775d9aac046e6e4148ef941b5927fedf9
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-26 23:30:31 -0700

    inverted_pytamid.java

---
+++ inverted_pytamid.java
@@ -0,0 +1,25 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = 5;
+		for(int i = r; i >= 1; --i) {
+			for(int j = 1; j <= i; ++j) {
+			System.out.print(j+" ");
+		}
+		System.out.println();
+
+	}
+
+}
+}
+
+/* output:
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+*/
