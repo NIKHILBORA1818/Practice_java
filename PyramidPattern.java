76b91e637c83eb96939bcb3c14f33dff9ca9ad28
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-26 22:26:31 -0700

    pyramid pattern

---
+++ PyramidPattern.java
@@ -0,0 +1,26 @@
+//pyramid pattern
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int r = 5;
+		for(int i = 0; i <= r; ++i) {
+			for(int j = 1; j <= i; ++j) {
+			System.out.print("+ ");
+		}
+		System.out.println();
+
+	}
+
+}
+}
+
+/* output:
+
++ 
++ + 
++ + + 
++ + + + 
++ + + + + 
+
+*/
