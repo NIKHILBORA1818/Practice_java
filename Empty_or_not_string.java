a68e1b8553b305c339e77ec2cf1918c82947dec5
Nikhil Bora <nikhilbora13@gmail.com>
2022-09-27 16:08:26 -0700

    Create Empty_or_not_string.java

---
+++ Empty_or_not_string.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "Nikhil";
+		String s2 = " ";
+		String s3 = null;
+		
+		System.out.println(s2.isEmpty());
+	}
+
+}
+
+/* output:
+false
+*/
